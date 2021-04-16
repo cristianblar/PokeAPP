e6d07e76cb5ce244e2af918742b99b12f7ff3ebb
Cristian Blandon <cristianblar12@icloud.com>
2021-04-15 20:30:58 -0500

    🗃 (db): Adds all the database models and its relations
    Completes pokemon model and adds type model. Creates the relationship between both models. Tested & working.

--- api/src/db.js
+++ api/src/db.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
@@ -35,10 +37,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Pokemon, Type } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Pokemon.belongsToMany(Type, { through: 'pokemon_types', timestamps: false });
+Type.belongsToMany(Pokemon, { through: 'pokemon_types', timestamps: false });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,51 @@
-const { DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    'pokemon',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      image_url: {
+        type: DataTypes.STRING,
+        defaultValue:
+          'https://res.cloudinary.com/cristianblar/image/upload/v1618532872/new-pokemon.jpg',
+      },
+      experience: {
+        type: DataTypes.INTEGER,
+      },
+      health: {
+        type: DataTypes.INTEGER,
+      },
+      attack: {
+        type: DataTypes.INTEGER,
+      },
+      defense: {
+        type: DataTypes.INTEGER,
+      },
+      speed: {
+        type: DataTypes.INTEGER,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+      },
     },
-  });
+    {
+      tableName: 'pokemons',
+      timestamps: false,
+    }
+  );
 };
