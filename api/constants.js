e5cef2c724c25e519fdb2e58cb7cef3ef1ae59df
Cristian Blandon <cristianblar12@icloud.com>
2021-04-22 19:28:41 -0500

    🍱 (backend): Updates assets and adds env example file
    Updates the URL for the created pokemon pictures and provides an env example file for deployment.

--- api/constants.js
+++ api/constants.js
@@ -4,7 +4,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const DB_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/pokemon`;
 const API_URL = `https://pokeapi.co/api/v2`;
-const NEW_IMG_URL = `https://res.cloudinary.com/cristianblar/image/upload/v1618532872/new-pokemon.jpg`;
+const NEW_IMG_URL = `https://res.cloudinary.com/cristianblar/image/upload/v1619137420/new-pokemon-conv_ba0fdx.webp`;
 const BACKEND_HOST = `http://localhost:3001`;
 
 module.exports = {
