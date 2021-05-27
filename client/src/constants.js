97b05935391dde9588e6fde00bfff29173e69edb
Cristian Blandon <cristianblar12@icloud.com>
2021-05-27 10:24:56 -0500

    👽 Updated API address to point to deployed backend

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 /* eslint-disable import/prefer-default-export */
-export const API_URL = `http://127.0.0.1:3001`;
+export const API_URL = `https://pokemon-app-back.herokuapp.com`;
 export const MAX_POKEMON_PAGE = 12;
