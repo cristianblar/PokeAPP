dc982e32f2e169443b6425bb580e0da40c7e7686
Cristian Blandon <cristianblar12@icloud.com>
2021-05-27 09:47:10 -0500

    🗃 Removes native support for PG

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -10,7 +10,7 @@ const { DB_URL } = require('../../constants');
 
 const sequelize = new Sequelize(DB_URL, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: true, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
