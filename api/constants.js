7a175c3631928db3285243d0e0f5885ed44822cd
Cristian Blandon <cristianblar12@icloud.com>
2021-05-27 07:26:36 -0500

    🚧 Adds Heroku config files for backend deployment
    Creates Procfile and modifies package.json

--- api/constants.js
+++ api/constants.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, PORT } = process.env;
 
 const DB_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/pokemon`;
 const API_URL = `https://pokeapi.co/api/v2`;
 const NEW_IMG_URL = `https://res.cloudinary.com/cristianblar/image/upload/v1619137420/AddPokemon/new-pokemon-conv_ba0fdx.webp`;
-const BACKEND_HOST = `http://localhost:3001`;
+const BACKEND_HOST = `http://localhost:${PORT}`;
 
 module.exports = {
+  PORT,
   DB_URL,
   API_URL,
   NEW_IMG_URL,
--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
-const server = require('./src/app.js');
-const { conn, addTypesToDb } = require('./src/db/index.js');
+const server = require('./src/app');
+const { conn, addTypesToDb } = require('./src/db/index');
+const { PORT } = require('./constants');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('Backend listening at 3001');
+  server.listen(PORT, () => {
+    console.log(`Backend listening at ${PORT}`);
     // Luego de levantar el server, inyectamos los Types a la DB desde la PokeAPI
     addTypesToDb()
       .then((resultado) => console.log(resultado))
